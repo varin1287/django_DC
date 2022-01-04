871a582b0330ce86cc62eb278035c8dc8b3a96c9
varin <zvv1287@mail.ru>
2022-01-04 13:46:54 +0300

    Добавлены модели

---
+++ my_blog/blog/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from .models import Category, Tag, Post, Comment
+
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Post)
+admin.site.register(Comment)
+
+
---
+++ my_blog/blog/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+
+class Category(models.Model):
+    """Модель категории"""
+    name = models.CharField('Имя', max_length=100)
+    slug = models.SlugField('url', max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Tag(models.Model):
+    """Модель тегов"""
+    name = models.CharField('Имя', max_length=100)
+    slug = models.SlugField(max_length=100, verbose_name='url')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'теги'
+
+
+class Post(models.Model):
+    """Модель постов"""
+    title = models.CharField(max_length=100, verbose_name='Название поста')
+    mini_text = models.TextField(max_length=200, verbose_name='Краткое описание')
+    text = models.TextField()
+    created_date_and_nbsp = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(max_length=100, verbose_name='url')
+
+
+class Comment(models.Model):
+    """Модель Коментариев"""
+    text = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    moderation = models.BooleanField()
+
